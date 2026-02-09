9d5c0bf9854e4acc40c6cc05ff7c262026356d8f
iListen2Sound <75883296+iListen2Sound@users.noreply.github.com>
2026-02-08 20:29:05 -0500

    Added basic classes for Controls

--- Controls.cs
+++ Controls.cs
@@ -6,7 +6,117 @@ using System.Threading.Tasks;
 
 namespace UIFramework
 {
-	internal class Controls
+	///<summary>
+	/// 
+	///</summary>
+	internal class Control
 	{
+		internal GameObject Go {get; set;}
+
+		internal void SetText()
+		{
+
+		}
+	}
+	///<summary>
+	/// 
+	///</summary>
+	internal class VTab : Control
+	{
+
+	}
+
+	///<summary>
+	/// 
+	///</summary>
+	internal class HTab : Control
+	{
+		
+	}
+
+	///<summary>
+	/// 
+	///</summary>
+	internal class Section : Control
+	{
+
+	}
+
+	///<summary>
+	/// 
+	///</summary>
+	internal class Button : Control
+	{
+
+	}
+
+	///<summary>
+	/// 
+	/// </summary>
+	internal class CheckBox : Control
+	{
+
+	}
+
+	///<summary>
+	/// 
+	///</summary>
+	internal class TextBox : Control
+	{
+
+	}
+
+	///<summary>
+	/// 
+	///</summary>
+	internal class TextBlock : Control
+	{
+
+	}
+
+	///<summary>
+	/// 
+	///</summary>
+	internal class LayoutPanel
+	{
+		internal void Add(Control control)
+		{
+
+		}
+	}
+
+	///<summary>
+	/// 
+	///</summary>
+	internal class ModList : LayoutPanel
+	{
+
+	}
+
+	///<summary>
+	/// 
+	///</summary>
+	internal class CategoryList : LayoutPanel
+	{
+
+	}
+
+	///<summary>
+	/// 
+	///</summary>
+	internal class BodyPanel : LayoutPanel
+	{
+
+	}
+
+	///<summary>
+	/// 
+	///</summary>
+	internal class Setting
+	{
+		internal string Description {get; set;}
+		internal Inputs InputType {get; set;}
+		internal string Identifier {get; set;}
+		
 	}
 }
---
+++ enums.cs
@@ -0,0 +1,11 @@
+public enum Inputs
+{
+    Checkbox,
+    TextBox,
+    //Button,  //Implement Checkbox and TExbox input for now
+    //DropDown,
+    //Slider,
+    //Numeric,
+
+
+}
