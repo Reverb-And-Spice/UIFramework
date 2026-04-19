0a79d53a0c9c94acfa943d4ebd8be1010f74c4d7
iListen2Sound <75883296+iListen2Sound@users.noreply.github.com>
2026-04-19 05:47:55 -0400

    Described initial interfaces for enhanced validator

---
+++ src/Validators/DefaultValidator.cs
@@ -0,0 +1,62 @@
+using MelonLoader;
+using MelonLoader.Logging;
+using MelonLoader.Preferences;
+using MonoMod.ModInterop;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System;
+using Tomlet;
+using Tomlet.Models;
+
+//using System;
+/*using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+using static UIFramework.Debug;
+using TMPro.TMP_InputField;
+namespace UIFramework.Validation
+{
+	public partial class UIFValidator : ValueValidator
+	{
+        public override bool IsValid(object value) {return true;}
+        public override object EnsureValid(object value) {return value;}
+    }
+
+    public interface ITextInputOptionsProvider
+    {
+        public  TMPro.TMP_InputField.ContentType ContentType {get; set;}
+        public bool IsRichText {get; set;}
+        public bool IsReadOnly {get; set;}
+        public int FontSize {get; set;}
+        public FontStyles FontStyle{get; set;}        
+    }
+
+    public interface IDynamicDropdownProvider
+    {
+        public List<string> DropdownOptionNames {get; set;}
+    }
+
+    public interface NumericSliderProvider
+    {
+        public int Min{get; set;}
+        public int Max{get; set;}
+    }
+
+    public interface NumericUpDownProvider
+    {
+        public float Increments {get; set;}
+    }
+
+    public interface InteractionEventProvider
+    {
+        public event action<EventArgs> Interaction;
+    }
+
+    public interface UifButton
+    {
+        public string ButtonText {get; set;}
+        public string Identifier {get; set;}
+        public string Description {get; set;} 
+    }
+
+}
