a6a357531882d68b2fa1d8cbb16263bd2c88f0e9
iListen2Sound <75883296+iListen2Sound@users.noreply.github.com>
2026-04-22 03:47:16 -0400

    Added more properties to ITextInputOptionsProvider

--- src/Validators/DefaultValidator.cs
+++ src/Validators/DefaultValidator.cs
@@ -18,45 +18,50 @@ namespace UIFramework.Validation
 {
 	public partial class UIFValidator : ValueValidator
 	{
-        public override bool IsValid(object value) {return true;}
-        public override object EnsureValid(object value) {return value;}
-    }
+		public override bool IsValid(object value) { return true; }
+		public override object EnsureValid(object value) { return value; }
+	}
 
-    public interface ITextInputOptionsProvider
-    {
-        public  TMP_InputField.ContentType ContentType {get; set;}
-        public bool IsRichText {get; set;}
-        public bool IsReadOnly {get; set;}
-        public int FontSize {get; set;}
-        public FontStyles FontStyle{get; set;}        
-    }
+	public interface ITextInputOptionsProvider
+	{
+		public TMP_InputField.ContentType ContentType { get; set; }
+		public int FontSize { get; set; }
+		public bool IsAutoSizing { get; set; }
+		public int AutoSizeMin { get; set; }
+		public int AutoSizeMax { get; set; }
+		public int CharacterLimit { get; set; }
+		public bool IsReadOnly { get; set; }
 
-    public interface IDynamicDropdownProvider
-    {
-        public List<string> DropdownOptionNames {get; set;}
-    }
+		public bool IsRichText { get; set; }
+		public FontStyles FontStyle { get; set; }
+	}
 
-    public interface NumericSliderProvider
-    {
-        public int Min{get; set;}
-        public int Max{get; set;}
-    }
+	public interface IDynamicDropdownProvider
+	{
+		public List<string> DropdownOptionNames { get; set; }
+	}
 
-    public interface NumericUpDownProvider
-    {
-        public float Increments {get; set;}
-    }
+	public interface NumericSliderProvider
+	{
+		public int Min { get; set; }
+		public int Max { get; set; }
+	}
 
-    public interface InteractionEventProvider
-    {
-        public event Action<EventArgs> Interaction;
-    }
+	public interface NumericUpDownProvider
+	{
+		public float Increments { get; set; }
+	}
 
-    public interface UifButton
-    {
-        public string ButtonText {get; set;}
-        public string Identifier {get; set;}
-        public string Description {get; set;} 
-    }
+	public interface InteractionEventProvider
+	{
+		public event Action<EventArgs> Interaction;
+	}
+
+	public interface UifButton
+	{
+		public string ButtonText { get; set; }
+		public string Identifier { get; set; }
+		public string Description { get; set; }
+	}
 
 }
